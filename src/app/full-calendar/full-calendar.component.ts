b1dced7da4534a023297fd5e8f95f0ace7a1fa2e
Tarasgroupki <kryvtsun16061996@gmail.com>
2020-09-01 16:47:09 +0300

    Added full-calendar localization

--- src/app/full-calendar/full-calendar.component.ts
+++ src/app/full-calendar/full-calendar.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnInit, ViewChild} from '@angular/core';
 // import { CalendarComponent } from 'ng-fullcalendar';
 import { CalendarOptions } from '@fullcalendar/angular';
+import esLocale from '@fullcalendar/core/locales/es';
+import ruLocale from '@fullcalendar/core/locales/ru';
 
 
 @Component({
@@ -11,13 +13,16 @@ import { CalendarOptions } from '@fullcalendar/angular';
 
 export class FullCalendarComponent implements OnInit {
   title = 'full-calendar';
+  locales = [esLocale, ruLocale];
   calendarOptions: CalendarOptions = {
     initialView: 'timeGridWeek',
     dateClick: this.handleDateClick.bind(this), // bind is important!
     events: [
-      { title: 'event 1', date: '2020-07-13 12:30:00' },
-      { title: 'event 2', date: '2020-07-13 13:30:00' }
-    ]
+      { title: 'event 1', date: '2020-09-01 12:30:00' },
+      { title: 'event 2', date: '2020-09-01 13:30:00' }
+    ],
+    locales: this.locales,
+    locale: 'ru'
   };
   // @ViewChild(FullCalendarComponent) ucCalendar: FullCalendarComponent;
 
